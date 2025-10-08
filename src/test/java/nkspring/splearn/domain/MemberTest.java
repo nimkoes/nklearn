55235931e4ad2a1f75dac312b73bdca2fea75be7
nimkoes <xxxelppa@gmail.com>
2025-10-08 16:05:31 +0900

    14. Member 도메인 로직 개발

--- src/test/java/nkspring/splearn/domain/MemberTest.java
+++ src/test/java/nkspring/splearn/domain/MemberTest.java
@@ -3,14 +3,61 @@ package nkspring.splearn.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberTest {
 
     @Test
     void createMember() {
         var member = new Member("nk@splearn.app", "nk", "secret");
-
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
+    @Test
+    void constructorNullCheck() {
+        assertThatThrownBy(() -> new Member(null, "nk", "secret"))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void activate() {
+        var member = new Member("nk@splearn.app", "nk", "secret");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        var member = new Member("nk@splearn.app", "nk", "secret");
+
+        member.activate();
+
+        assertThatThrownBy(() -> {
+            member.activate();
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate() {
+        var member = new Member("nk@splearn.app", "nk", "secret");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        var member = new Member("nk@splearn.app", "nk", "secret");
+
+        assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
+    }
 }
