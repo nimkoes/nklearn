01d543065e22ff81c40ecb84772a44b64d20826d
nimkoes <xxxelppa@gmail.com>
2025-10-10 23:40:05 +0900

    25. 엔티티의 자연키 지정

--- src/test/java/nkspring/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/nkspring/splearn/application/required/MemberRepositoryTest.java
@@ -5,10 +5,12 @@ import nkspring.splearn.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static nkspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static nkspring.splearn.domain.MemberFixture.createPasswordEncoder;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 class MemberRepositoryTest {
@@ -29,5 +31,16 @@ class MemberRepositoryTest {
         assertThat(member.getId()).isNotNull();
 
         entityManager.flush();
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
     }
 }
