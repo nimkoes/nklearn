4ea225c77c22cee6df081c677693208096ff718d
nimkoes <xxxelppa@gmail.com>
2025-10-12 16:41:51 +0900

    35. 회원 상세 정보 도메인 개발

--- src/main/java/nkspring/splearn/domain/Member.java
+++ src/main/java/nkspring/splearn/domain/Member.java
@@ -24,6 +24,8 @@ public class Member extends AbstractEntity {
 
     private MemberStatus status;
 
+    private MemberDetail detail;
+
     public static Member register(MemberRegisterRequest registerRequest, PasswordEncoder passwordEncoder) {
         Member member = new Member();
 
