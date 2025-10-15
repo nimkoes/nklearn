933d7b39d0a675cf88e4f0fba8f5ac84f413c4e3
nimkoes <xxxelppa@gmail.com>
2025-10-15 21:12:42 +0900

    42. API 테스트와 ProblemDetail 예외 핸들러 개발

---
+++ src/test/java/nkspring/splearn/AssertThatUtils.java
@@ -0,0 +1,19 @@
+package nkspring.splearn;
+
+import nkspring.splearn.domain.member.MemberRegisterRequest;
+import org.assertj.core.api.AssertProvider;
+import org.springframework.test.json.JsonPathValueAssert;
+
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AssertThatUtils {
+    public static Consumer<AssertProvider<JsonPathValueAssert>> notnull() {
+        return value -> assertThat(value).isNotNull();
+    }
+
+    public static Consumer<AssertProvider<JsonPathValueAssert>> equalsTo(MemberRegisterRequest request) {
+        return value -> assertThat(value).isEqualTo(request.email());
+    }
+}
